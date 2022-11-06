19c90a996411b56e66b87d22ade6b700353dad40
Doroteo111 <ladoritaesta@gmail.com>
2022-11-07 00:17:41 +0100

    prueba ejercicio1 (sale mal)

---
+++ Assets/Scenes/SCRIPTS/EXERCISE_1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public float x = 3f;
+public float y = 5f;
+public float z = 7f;
+[SerializeField]private float result;
+public class EXERCISE_1 : MonoBehaviour
+{
+   Debug.Log(result=x + y + z);
+
+}
