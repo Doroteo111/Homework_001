1ef153d46409261df40e8f428dc9359a9c81d8e4
Doroteo111 <ladoritaesta@gmail.com>
2022-11-08 11:25:08 +0100

    exercise6 done

--- Assets/Scenes/SCRIPTS/EXERCISE_6.cs
+++ Assets/Scenes/SCRIPTS/EXERCISE_6.cs
@@ -8,13 +8,14 @@ public class EXERCISE_6 : MonoBehaviour
     //VARIABLE ENTERA QUE GARDA LOS NUMEROS. en base que numeros agrege a cada variable va dentro de x o y
     //operador de decisiones
 
-    public bool isWeekend;
+    /*public bool isWeekend; */
 
     public float dayWeek = 1;
+    
 
     private void Start()
     {
-        if (isWeekend == false)
+        if (dayWeek >= 6 )
         {
             Debug.Log($"Day {dayWeek} belongs to the weekend");
 
