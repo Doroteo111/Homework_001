b16bd39e9a3a97bccf2e7f80e4087990177395bb
Doroteo111 <ladoritaesta@gmail.com>
2022-11-20 21:48:30 +0100

    fixed exercise 10

--- Assets/Scenes/SCRIPTS/EXERCISE_10.cs
+++ Assets/Scenes/SCRIPTS/EXERCISE_10.cs
@@ -8,15 +8,15 @@ public class EXERCISE_10 : MonoBehaviour
  
     private void Start()
     {
-        Debug.Log($"2 x 1 = {x * 1}");  //We repeat the same pattern to get the multiplication table of 2
-        Debug.Log($"2 x 2 = {x * 2}");
-        Debug.Log($"2 x 3 = {x * 3}");
-        Debug.Log($"2 x 4 = {x * 4}");
-        Debug.Log($"2 x 5 = {x * 5}");
-        Debug.Log($"2 x 6 = {x * 6}");
-        Debug.Log($"2 x 7 = {x * 7}");
-        Debug.Log($"2 x 8 = {x * 8}");
-        Debug.Log($"2 x 9 = {x * 9}");
-        Debug.Log($"2 x 10 = {x * 10}"); 
+        Debug.Log($"¨{x} x 1 = {x * 1}");  //We repeat the same pattern to get the multiplication table of 2
+        Debug.Log($"{x}x 2 = {x * 2}");
+        Debug.Log($"{x} x 3 = {x * 3}");
+        Debug.Log($"{x}x 4 = {x * 4}");
+        Debug.Log($"{x} x 5 = {x * 5}");
+        Debug.Log($"{x} x 6 = {x * 6}");
+        Debug.Log($"{x} x 7 = {x * 7}");
+        Debug.Log($"{x}x 8 = {x * 8}");
+        Debug.Log($"{x} x 9 = {x * 9}");
+        Debug.Log($"{x} x 10 = {x * 10}"); 
     }
 }
